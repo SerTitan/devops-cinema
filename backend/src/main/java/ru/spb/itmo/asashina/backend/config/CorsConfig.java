4530ffe44873776037d52edbcf97e9e8f8fd8f6d
a.sashina <yaninastya2010@yandex.ru>
2025-05-22 20:45:39 +0300

    Расширение cors конфига

--- backend/src/main/java/ru/spb/itmo/asashina/backend/config/CorsConfig.java
+++ backend/src/main/java/ru/spb/itmo/asashina/backend/config/CorsConfig.java
@@ -3,6 +3,7 @@ package ru.spb.itmo.asashina.backend.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -16,7 +17,9 @@ public class CorsConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(authorizeRequests -> authorizeRequests.anyRequest()
-                .permitAll());
+                .permitAll())
+                .cors(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable);
         return http.build();
     }
 
